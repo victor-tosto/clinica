1db20e9db4e3476683a4d79041334c4cbc387aee
Victor Morales <victormoralestosto@hotmail.com>
2023-07-19 22:17:50 -0300

    dia 2 finalizado

---
+++ assets/js/btnFooter.js
@@ -0,0 +1,15 @@
+const btnMore = document.querySelector('.button-area__more');
+const btns = document.querySelectorAll('.button-area');
+const btnArea = document.querySelector('.footer__button-area')
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+const activeEvent = [btnArea, overlay];
+
+
+btnMore.addEventListener('click', () => {
+
+    body.classList.toggle('suppress-scroll')
+    btns.forEach(btn => btn.classList.toggle('open'));
+    activeEvent.forEach(btn => btn.classList.toggle('open'));
+
+})
